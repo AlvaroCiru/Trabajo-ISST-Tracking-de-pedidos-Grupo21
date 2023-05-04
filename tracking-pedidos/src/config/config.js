2388ab2a9d09e20b3a141fef1694cec26d4294b6
Alberto Rebolledo <alberto.rebolledo.j@gmail.com>
2023-05-04 23:20:07 +0200

    Funciona correcto

--- tracking-pedidos/src/config/config.js
+++ tracking-pedidos/src/config/config.js
@@ -1,5 +1,6 @@
 const CONFIG = {
-    users_url: "http://localhost:8083/tracking/api/usuarios",  
+    users_url: "http://localhost:8083/tracking/api/usuarios",
+    pedidos_url: "http://localhost:8083/tracking/api/pedidos",
     use_server: true,
     loading_timeout_ms: 300
   }
