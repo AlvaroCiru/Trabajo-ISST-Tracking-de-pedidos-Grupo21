3c8239bc59f0faa721746b9b7c462b363baa4d25
javiermonzon <j.monzon@alumnos.upm.es>
2023-05-05 19:04:53 +0200

    Commit Pruebas JUnit5

---
+++ backend/src/test/java/es/upm/dit/isst/backend/BackendTest.java
@@ -0,0 +1,108 @@
+package es.upm.dit.isst.backend;
+
+
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import es.upm.dit.isst.backend.model.Direccion;
+import es.upm.dit.isst.backend.model.Empresa;
+import es.upm.dit.isst.backend.model.Usuario;
+import es.upm.dit.isst.backend.repository.UsuarioRepository;
+
+
+
+@SpringBootTest
+class Backend {
+
+	@Autowired
+	UsuarioRepository repo;
+
+	@Test
+	void prueba1(){
+		boolean a = true;
+		Assertions.assertTrue(a);
+	}
+
+	@Test
+	void compruebaSetNombreUsuario() {
+		Direccion direccion1 = new Direccion(0, 25.3, 0.29, "Cuenca", "Tarancon", "calle santo tomas", "28698");
+		Empresa empresa1 = new Empresa(0, "empresa1", "empresa1@gmail.com", "62512555", direccion1);
+		Usuario usu1 = new Usuario(7, "raul", "raul@gmail.com", "contra", "69785695", false, empresa1);
+		Usuario usu2 = new Usuario();
+
+		usu2.setNombre("alberto");
+
+		System.out.println(usu1.getNombre());
+		System.out.println(usu2.getNombre());
+
+		Assertions.assertTrue(usu1.getNombre()==usu2.getNombre());
+	}
+
+	@Test
+	void compruebaSetContrasenaUsuario() {
+		Direccion direccion1 = new Direccion(0, 25.3, 0.29, "Cuenca", "Tarancon", "calle santo tomas", "28698");
+		Empresa empresa1 = new Empresa(0, "empresa1", "empresa1@gmail.com", "62512555", direccion1);
+		Usuario usu1 = new Usuario(7, "raul", "raul@gmail.com", "contra", "69785695", false, empresa1);
+		Usuario usu2 = new Usuario();
+
+		usu2.setContrasena("pass");
+
+		System.out.println(usu1.getContrasena());
+		System.out.println(usu2.getContrasena());
+
+		Assertions.assertTrue(usu1.getContrasena()==usu2.getContrasena());
+	}
+
+	@Test
+	void compruebaSetEmailUsuario() {
+		Direccion direccion1 = new Direccion(0, 25.3, 0.29, "Cuenca", "Tarancon", "calle santo tomas", "28698");
+		Empresa empresa1 = new Empresa(0, "empresa1", "empresa1@gmail.com", "62512555", direccion1);
+		Usuario usu1 = new Usuario(7, "raul", "raul@gmail.com", "contra", "69785695", false, empresa1);
+		Usuario usu2 = new Usuario();
+
+		usu2.setEmail("j1@gmail.com");
+
+		System.out.println(usu1.getEmail());
+		System.out.println(usu2.getEmail());
+
+		Assertions.assertTrue(usu1.getEmail()==usu2.getEmail());
+	}
+
+	@Test
+	void compruebaSetTelefonoUsuario() {
+		Direccion direccion1 = new Direccion(0, 25.3, 0.29, "Cuenca", "Tarancon", "calle santo tomas", "28698");
+		Empresa empresa1 = new Empresa(0, "empresa1", "empresa1@gmail.com", "62512555", direccion1);
+		Usuario usu1 = new Usuario(7, "raul", "raul@gmail.com", "contra", "69785695", false, empresa1);
+		Usuario usu2 = new Usuario();
+
+		usu2.setTelefono("5985885");
+
+		System.out.println(usu1.getTelefono());
+		System.out.println(usu2.getTelefono());
+
+		Assertions.assertTrue(usu1.getTelefono()==usu2.getTelefono());
+	}
+
+	@Test
+	void compruebaFindByIdNombre(){
+		Direccion direccion1 = new Direccion(0, 25.3, 0.29, "Cuenca", "Tarancon", "calle santo tomas", "28698");
+		Empresa empresa1 = new Empresa(0, "empresa1", "empresa1@gmail.com", "62512555", direccion1);
+		Usuario usu1 = new Usuario(7, "raul", "raul@gmail.com", "contra", "69785695", false, empresa1);
+		repo.save(usu1);
+		List<Usuario> a = repo.findByNombre("raul");
+		boolean resultado = a.isEmpty();
+		Assertions.assertFalse(resultado);
+	}
+
+
+
+
+
+
+
+
+}
