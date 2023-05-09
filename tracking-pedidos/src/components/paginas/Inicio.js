85418d8179a6164a6f2849cbbad7bf8a9447b67e
Alberto Rebolledo <alberto.rebolledo.j@gmail.com>
2023-05-09 03:03:40 +0200

    En la página principal hay un botón para ver tus pedidos, si no estás logged, te lleva a login

--- tracking-pedidos/src/components/paginas/Inicio.js
+++ tracking-pedidos/src/components/paginas/Inicio.js
@@ -1,12 +1,30 @@
 import React, { useEffect, useState } from "react";
 import "./../../style/Inicio.css"
 import Texto from "../Texto";
-
+import { useNavigate } from "react-router-dom";
 
 export default function Inicio(props){
+
+    const navigate = useNavigate()
+    const user = props.sesion ? JSON.parse(props.sesion) : null
+
+    const handleClick = () => {
+        if(user) {
+            if(user.es_gestor) {
+                navigate(`/gestor/${user.id}/pedidos`);
+            } else {
+                navigate(`/comprador/${user.id}/pedidos`)
+            }
+        } else {
+            navigate("/login")
+        }
+
+    }
+
     return(
-        <div>
+        <div className="inicio">
             <h2 className="title">Inicia Sesión para ver tus pedidos</h2>
+            <button className="misPedidosButton" onClick={handleClick}>Ver Mis Pedidos</button>
             <div className="textos">
                 <div className="texto"><Texto title="¿Quiénes somos?" /></div>
                 <div className="texto"><Texto title="Instrucciones" /></div>
